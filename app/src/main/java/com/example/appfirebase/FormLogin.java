6f8d0f03d1462535f8617fe25ee5fc8d85465f2c
Mateus Cavalcanti <mateusmartinscg@gmail.com>
2024-06-07 11:41:54 -0300

    frist commit

--- app/src/main/java/com/example/appfirebase/FormLogin.java
+++ app/src/main/java/com/example/appfirebase/FormLogin.java
@@ -5,9 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class FormLogin extends AppCompatActivity {
-
+    private TextView textTelaCadastro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
