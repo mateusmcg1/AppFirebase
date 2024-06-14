0ff0d3258b05b24010a1b6ecfe964098d7600113
Mateus Cavalcanti <mateusmartinscg@gmail.com>
2024-06-14 19:31:07 -0300

    new features

--- app/src/main/java/com/example/appfirebase/FormCadastro.java
+++ app/src/main/java/com/example/appfirebase/FormCadastro.java
@@ -1,6 +1,9 @@
 package com.example.appfirebase;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class FormCadastro extends AppCompatActivity {
-
+    private ImageButton imageButtonVoltar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_form_cadastro);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        this.voltarLogin();
+        }
+        public void voltarLogin(){
+            this.imageButtonVoltar = findViewById(R.id.ButtonBackCadastro);
+            imageButtonVoltar.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(FormCadastro.this, FormLogin.class);
+                    startActivity(intent);
+                }
+            });
+        }
     }
-}
--- app/src/main/java/com/example/appfirebase/FormLogin.java
+++ app/src/main/java/com/example/appfirebase/FormLogin.java
@@ -2,6 +2,7 @@ package com.example.appfirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
@@ -15,5 +16,22 @@ public class FormLogin extends AppCompatActivity {
         setContentView(R.layout.activity_form_login);
         ProgressBar progressBar = findViewById(R.id.progressBar);
         progressBar.setVisibility(View.INVISIBLE);
+        textTelaCadastro = findViewById(R.id.TextViewCadastrarLogin);
+        this.chamaTelaCadastro();
     }
+
+    public void chamaTelaCadastro(){
+        textTelaCadastro = findViewById(R.id.TextViewCadastrarLogin);
+        textTelaCadastro.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FormLogin.this, FormCadastro.class);
+                startActivity(intent);
+            }
+        });
+
+        }
+
+
 }
